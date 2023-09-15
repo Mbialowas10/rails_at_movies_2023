f43b479c8c592a0fd10c3b3a88f14a4865668738
mbialowas10 <mbialowas@rrc.ca>
2023-09-15 14:26:31 -0500

    Added Movies and ProductionComapnies controllers, so that we can act like a traffic guard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'production_companies/index'
+  get 'production_companies/show'
+  get 'movies/index'
+  get 'movies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
