907ba99ae7be91924d11e52433dbe5e03f676f21
mbialowas10 <mbialowas@rrc.ca>
2023-09-19 14:16:53 -0500

    Added Styling with Bulma

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,11 @@
 class MoviesController < ApplicationController
   def index
+    # @movies = Movie.all # this creates the N+1 problem; +1 refers to movies whereas N refers to production_companies,
+    # in short this means we are reduntantly calling the database which can be expensive :)
+    @movies = Movie.includes(:production_company).all.order('average_vote DESC')
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'production_companies/index'
-  get 'production_companies/show'
-  get 'movies/index'
-  get 'movies/show'
+  # get 'production_companies/index'
+  # get 'production_companies/show'
+  # get 'movies/index'
+  # get 'movies/show'
+  resources :movies, only: [:index, :show]
+  resources :production_companies, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
